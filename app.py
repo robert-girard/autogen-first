a99b075d0fd012c00447919ace9d37e132d1db27
robert.girard <r_girard@live.com>
2023-11-17 22:09:35 -0500

    basic usage

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import autogen
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
 
 config_list = [
     {
-        'model': 'gpt-4',
-        'api_key': 'API_KEY'
+        'model': 'gpt-4-1106-preview',
+        'api_key': OPENAI_KEY
     }
 ]
 
@@ -17,13 +24,13 @@ llm_config={
 assistant = autogen.AssistantAgent(
     name="CTO",
     llm_config=llm_config,
-    system_message="Chief technical officer of a tech company"
+    system_message="Expert webscraper and data analyst for retail product information"
 )
 
 user_proxy = autogen.UserProxyAgent(
     name="user_proxy",
     human_input_mode="NEVER",
-    max_consecutive_auto_reply=10,
+    max_consecutive_auto_reply=15,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
     code_execution_config={"work_dir": "web"},
     llm_config=llm_config,
@@ -32,19 +39,10 @@ Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
 )
 
 task = """
-Write python code to output numbers 1 to 100, and then store the code in a file
+Find me a good deal on a GPU capable of running local large langauge models (LLms) with atleast 30b parameters and no quantization. Give me a graph of the price of the gpu over time from atleast 3 sources and dating back atleast 6 months.
 """
 
 user_proxy.initiate_chat(
     assistant,
     message=task
-)
-
-task2 = """
-Change the code in the file you just created to instead output numbers 1 to 200
-"""
-
-user_proxy.initiate_chat(
-    assistant,
-    message=task2
 )
